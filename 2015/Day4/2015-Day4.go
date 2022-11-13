fc037057b2c8d26c8132d894423a61e9f73a4b58
danielreedy21 <dreedy@luc.edu>
2022-11-12 23:50:49 -0600

    finished Day 4

--- 2015/Day4/2015-Day4.go
+++ 2015/Day4/2015-Day4.go
@@ -2,4 +2,55 @@ package main
 
 import (
 	"fmt"
+	"crypto/md5"
+	"strconv"
 )
+
+func main() {
+	secretKey := "yzbqklnj"
+	fmt.Println(findAnswer(secretKey))
+}
+
+func findAnswer(secretKey string) int {
+	currInt := 7406921
+	for true {
+		currIntStr := strconv.Itoa(currInt)
+		currSecretKey := secretKey+currIntStr
+		fmt.Println(currSecretKey)
+		if checkZeros(grabMD5(currSecretKey)){
+			return currInt
+		}
+		currInt++
+	}
+	return 0
+}
+
+func grabMD5(input string) [16]byte {
+	inputBytes := []byte(input)
+	md5bytes := md5.Sum(inputBytes)
+	return md5bytes
+}
+
+func checkZeros(md5input [16]byte) bool {
+	if md5input[0]==0 && md5input[1]==0 && md5input[2]==0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+
+
+
+func convToHexString(md5input [16]byte) string {
+	outputStr := ""
+	for i:=0;i<len(md5input);i++{
+		byteAsInt := int64(md5input[i])
+		hexByte := strconv.FormatInt(byteAsInt, 16)
+		if byteAsInt < 10 {
+			hexByte = "0" + hexByte
+		}
+		outputStr = outputStr + hexByte
+	}
+	return outputStr
+}
