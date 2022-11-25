e23ccf3cb42c23c488c16a8b0460fffc192c18d4
danielreedy21 <dreedy@luc.edu>
2022-11-24 23:19:03 -0600

    prettier operator functions

--- 2015/Day7/2015-Day7.go
+++ 2015/Day7/2015-Day7.go
@@ -8,49 +8,52 @@ import (
 
 func main() {
 	startTime := time.Now()
-	x := [16]int{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1}
-	y := [16]int{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 0}
-	fmt.Println("Running Main ...")
-	fmt.Println("x is :")
-	fmt.Println(x)
-	fmt.Println("y is :")
-	fmt.Println(y)
-	fmt.Println("x and y is:")
-	fmt.Println(and(x, y))
-	fmt.Println("x or y is:")
-	fmt.Println(or(x, y))
-	fmt.Println("16 byte of 234 is :")
-	fmt.Println(intToByte(234))
-	fmt.Println("x as an int is:")
-	fmt.Println(byteToInt(x))
 
+	// actual executables
+	fmt.Print("Running Main ...\n\n\n")
+	x := 123
+	y := 456
+	fmt.Println("x is", x, "\ny is", y)
+	fmt.Println("x and y is:", and(x, y))
+	fmt.Println("x or y is:", or(x, y))
+	fmt.Println("x LSHIFT 2 is:", lshift(x, 2))
+	fmt.Println("y RSHIFT 2 is:", rshift(y, 2))
+	fmt.Println("not x is:", not(x))
+	fmt.Println("not y is:", not(y))
+
+	// calculate execution time
 	endTime := time.Now()
 	runTime := endTime.Sub(startTime)
-	fmt.Println("\n\n\nExecution Time: ")
+	fmt.Println("\n\nExecution Time: ")
 	fmt.Println(runTime)
 }
 
-func and(byte1 [16]int, byte2 [16]int) [16]int {
+func and(num1 int, num2 int) int {
+	byte1 := intToByte(num1)
+	byte2 := intToByte(num2)
 	var newByte [16]int
 	for i := 0; i < len(byte1); i++ {
 		if byte1[i] == 1 && byte2[i] == 1 {
 			newByte[i] = 1
 		}
 	}
-	return newByte
+	return byteToInt(newByte)
 }
 
-func or(byte1 [16]int, byte2 [16]int) [16]int {
+func or(num1 int, num2 int) int {
+	byte1 := intToByte(num1)
+	byte2 := intToByte(num2)
 	var newByte [16]int
 	for i := 0; i < len(byte1); i++ {
 		if byte1[i] == 1 || byte2[i] == 1 {
 			newByte[i] = 1
 		}
 	}
-	return newByte
+	return byteToInt(newByte)
 }
 
-func lshift(byte1 [16]int, shiftNum int) [16]int {
+func lshift(num1 int, shiftNum int) int {
+	byte1 := intToByte(num1)
 	var newByte [16]int
 	//this adds zeros to the nth postitions on the right side of the array
 	for i := 1; i <= shiftNum; i++ {
@@ -60,10 +63,11 @@ func lshift(byte1 [16]int, shiftNum int) [16]int {
 	for i := 0; i < len(newByte)-shiftNum; i++ {
 		newByte[i] = byte1[i+shiftNum]
 	}
-	return newByte
+	return byteToInt(newByte)
 }
 
-func rshift(byte1 [16]int, shiftNum int) [16]int {
+func rshift(num1 int, shiftNum int) int {
+	byte1 := intToByte(num1)
 	var newByte [16]int
 	//this adds zeros to the nth postitions on the left side of the array
 	for i := 0; i < shiftNum; i++ {
@@ -73,32 +77,24 @@ func rshift(byte1 [16]int, shiftNum int) [16]int {
 	for i := shiftNum; i < len(newByte); i++ {
 		newByte[i] = byte1[i-shiftNum]
 	}
-	return newByte
+	return byteToInt(newByte)
 }
 
-func not(byte1 [16]int) [16]int {
+func not(num1 int) int {
+	byte1 := intToByte(num1)
 	var newByte [16]int
 	for i := 0; i < len(newByte); i++ {
 		if byte1[i] == 0 {
 			newByte[i] = 1
 		}
 	}
-	return newByte
-}
-
-func byteToNum(byte1 [16]int) int {
-	sum := 0
-	for i := 0; i < len(byte1); i++ {
-		if byte1[i] == 1 {
-			sum = sum + int(math.Pow(2, float64(15-i)))
-		}
-	}
-	return sum
+	return byteToInt(newByte)
 }
 
 func intToByte(num int) [16]int {
 	var newByte [16]int
 	difference := num
+	// this is like a while loop
 	for difference > 0 {
 		for i := 0; i < len(newByte); i++ {
 			currPow := 15 - i
