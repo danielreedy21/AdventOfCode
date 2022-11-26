8f4626faf71102c979045aee3149c3807538dbbc
danielreedy21 <dreedy@luc.edu>
2022-11-26 00:10:49 -0600

    finished day 7

--- 2015/Day7/2015-Day7.go
+++ 2015/Day7/2015-Day7.go
@@ -1,16 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"math"
+	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
 	startTime := time.Now()
 
-	// actual executables
+	// test cases for my bitwise functions
 	fmt.Print("Running Main ...\n\n\n")
+	fmt.Println("Bitwise function tests: ")
 	x := 123
 	y := 456
 	fmt.Println("x is", x, "\ny is", y)
@@ -21,6 +27,126 @@ func main() {
 	fmt.Println("not x is:", not(x))
 	fmt.Println("not y is:", not(y))
 
+	// answering part 1
+	// for part 2, just switch b's signal (line 90) from 14146 to 956
+
+	//reads input
+	file, err := os.Open("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	var lines [339]string
+	i := 0
+	for scanner.Scan() {
+		lines[i] = scanner.Text()
+		i++
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	// these maps house the calculated wires and the wires that still just hold instructions
+	calcWires := map[string]int{}
+	instrWires := map[string][]string{}
+
+	// iterate over input lines
+	for i := 0; i < len(lines); i++ {
+		parsedLine := parseString(lines[i])
+		wireName := parsedLine[len(parsedLine)-1]
+		// fmt.Println(parsedLine, len(parsedLine))
+
+		// checks if the line on the input is a number while assigning that number to a variable
+		// effectively, this grabs the initial assignments
+		if num, err := strconv.Atoi(parsedLine[0]); err == nil && len(parsedLine) == 3 {
+			calcWires[wireName] = num
+		} else {
+			instrSlice := parsedLine[0 : len(parsedLine)-2]
+			instrWires[wireName] = instrSlice
+		}
+	}
+
+	// while there are remaining instrucitons, execute the instructions that can be executed
+	for len(instrWires) > 0 {
+		for key, instruction := range instrWires {
+
+			// case assigning
+			if len(instruction) == 1 {
+				if num, ok := calcWires[instruction[0]]; ok {
+					calcWires[key] = num
+					delete(instrWires, key)
+				}
+
+				//case NOT
+			} else if instruction[0] == "NOT" {
+				// if instuction variable exists in calcWires, execute command
+				if num, ok := calcWires[instruction[1]]; ok {
+					calcWires[key] = not(num)
+					delete(instrWires, key)
+				}
+
+				// case AND
+			} else if instruction[1] == "AND" {
+				num1, ok1 := calcWires[instruction[0]]
+				num2, ok2 := calcWires[instruction[2]]
+				int1, err1 := strconv.Atoi(instruction[0])
+				int2, err2 := strconv.Atoi(instruction[2])
+				if ok1 && ok2 {
+					calcWires[key] = and(num1, num2)
+					delete(instrWires, key)
+				} else if ok1 && err2 == nil {
+					calcWires[key] = and(num1, int2)
+					delete(instrWires, key)
+				} else if err1 == nil && err2 == nil {
+					calcWires[key] = and(int1, int2)
+					delete(instrWires, key)
+				} else if err1 == nil && ok2 {
+					calcWires[key] = and(int1, num2)
+					delete(instrWires, key)
+				}
+
+				// case OR
+			} else if instruction[1] == "OR" {
+				num1, ok1 := calcWires[instruction[0]]
+				num2, ok2 := calcWires[instruction[2]]
+				int1, err1 := strconv.Atoi(instruction[0])
+				int2, err2 := strconv.Atoi(instruction[2])
+				if ok1 && ok2 {
+					calcWires[key] = or(num1, num2)
+					delete(instrWires, key)
+				} else if ok1 && err2 == nil {
+					calcWires[key] = or(num1, int2)
+					delete(instrWires, key)
+				} else if err1 == nil && err2 == nil {
+					calcWires[key] = or(int1, int2)
+					delete(instrWires, key)
+				} else if err1 == nil && ok2 {
+					calcWires[key] = or(int1, num2)
+					delete(instrWires, key)
+				}
+
+				// case LSHIFT
+			} else if instruction[1] == "LSHIFT" {
+				if num, ok := calcWires[instruction[0]]; ok {
+					shiftNum, _ := strconv.Atoi(instruction[2])
+					calcWires[key] = lshift(num, shiftNum)
+					delete(instrWires, key)
+				}
+
+				// case RSHIFT
+			} else if instruction[1] == "RSHIFT" {
+				if num, ok := calcWires[instruction[0]]; ok {
+					shiftNum, _ := strconv.Atoi(instruction[2])
+					calcWires[key] = rshift(num, shiftNum)
+					delete(instrWires, key)
+				}
+			}
+		}
+	}
+
+	fmt.Printf("\n\nwire A value is: %v", calcWires["a"])
+
 	// calculate execution time
 	endTime := time.Now()
 	runTime := endTime.Sub(startTime)
@@ -28,6 +154,13 @@ func main() {
 	fmt.Println(runTime)
 }
 
+// need this function to split input lines into an array of strings
+func parseString(line string) []string {
+	parsedString := strings.Split(line, " ")
+	return parsedString
+}
+
+// BITWISE OPERATIONS
 func and(num1 int, num2 int) int {
 	byte1 := intToByte(num1)
 	byte2 := intToByte(num2)
